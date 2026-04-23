c90946a14b5e4fb3c175a1e769856c885622ddd7
sardarswanand <swanand.sardar0693@gmail.com>
2026-04-23 20:46:12 +0530

    Update greeting message with salary offer

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!oooooooooooooo My name Swanand I am DevOps Engineer");
+        System.out.println("Hello, World!oooooooooooooo My name Swanand I am DevOps Engineer with offer of 20LPA");
     }
 }
